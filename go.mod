1e26e62ad6822081e424fa8ab32334b9aab8d11e
Rafaeros <rafaeladriancordeirocosta@gmail.com>
2024-10-17 19:48:32 -0300

    Iniciando configuração do turso sqlite3

--- go.mod
+++ go.mod
@@ -6,3 +6,11 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.24
 )
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
+	github.com/tursodatabase/go-libsql v0.0.0-20241011135853-3effbb6dea5c // indirect
+	golang.org/x/exp v0.0.0-20230515195305-f3d0a9c9a5cc // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"os"
+	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"database/sql"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/tursodatabase/go-libsql"
+
 	"github.com/rafaeros/fk-api/api/routers"
 )
 
 func main() {
 	basePath := "/api/v1"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		os.Exit(1)
+	}
+
+	log.Println("Get environment variables")
+
+	authToken := os.Getenv("TURSO_AUTH_TOKEN")
+	primaryUrl := os.Getenv("TURSO_FK_URL")
+	dbName := os.Getenv("TURSO_FK_DATABASE")
+
+	dir, err := os.MkdirTemp("", "libsql-*")
+	if err != nil {
+		fmt.Println("Error creating temporary directory:", err)
+        os.Exit(1)
+	}
+	defer os.RemoveAll(dir)
+
+	dbPath := filepath.Join(dir, dbName)
+
+	connector, err := libsql.NewEmbeddedReplicaConnector(dbPath, primaryUrl, libsql.WithAuthToken(authToken))
+	if err != nil {
+        fmt.Println("Error creating connector:", err)
+        os.Exit(1)
+    }
+	defer connector.Close()
+
+	db := sql.OpenDB(connector)
+    defer db.Close()
 
 	r := mux.NewRouter()
 		
 	log.Println("Setting up routes")
 	r = routers.RoutersOrdemProducao(r, basePath)
-
 
 	log.Println("Setting up handlers")
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
